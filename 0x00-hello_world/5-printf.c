33871a045731fc90b84aa7d32fb7581a9df74b92
Zizamaranga <marangaziza@gmail.com>
2023-10-06 19:16:18 +0300

    d

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  *
  * description: prints the said characters
  *
- * return: returns to 0 (success)
- *
+ * return: Always returns to 0 (success)
+ * return main : returns to 0 (success)
  */
 
-int main(void) /* main - This is the entry point of the function */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
