dab0fa5115d82b438b9d2e86e507a3de80b87864
Zizamaranga <marangaziza@gmail.com>
2023-10-06 12:15:11 +0300

    v

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
  
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
+        puts("\"Programming is like building a multilingual puzzle\n");:1
         return (0);
 
 }
