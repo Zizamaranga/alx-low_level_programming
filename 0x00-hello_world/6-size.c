8a72c6d75c650143d49c3a3175c8d6dd4e8eb46e
Zizamaranga <marangaziza@gmail.com>
2023-10-06 19:00:09 +0300

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,13 +12,10 @@ int main(void)
 {
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a short: %zu byte(s)\n", sizeof(short));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long: %zu byte(s)\n", sizeof(long));
-	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	printf("Size of a double: %zu byte(s)\n", sizeof(double));
-	printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 
 	return (0);
 }
