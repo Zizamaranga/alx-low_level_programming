13de99ece7a7a849a2f3c6bdb5a87fdbcc0ce680
Zizamaranga <marangaziza@gmail.com>
2023-10-06 11:02:32 +0300

    df

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *Main : entry point
+ *prints the said characters
+ *returns to 0_ success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
