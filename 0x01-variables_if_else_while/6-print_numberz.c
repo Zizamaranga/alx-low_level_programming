bb21d519bb8a96e3ed00c379d534695a0b469110
Zizamaranga <marangaziza@gmail.com>
2023-10-08 11:38:00 +0300

    f

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints al  single digits of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+
+	{
+		putchar(n);
+		n++;
+	}
+	{
+		putchar('\n');
+		return (0);
+	}
+}
+
