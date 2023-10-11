16bbd486b66b6e42bc2025c8a3691a06d9686e4c
Zizamaranga <marangaziza@gmail.com>
2023-10-11 17:09:52 +0300

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point 
+ * 
+ * Description - Prints Putchar followed by a new line 
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	putchar("_putchar\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * void -Entry Point
+ * description - prints alphabet
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+
+void print_alphabet(void);
+{
+	int n = 'a';
+	while(a <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	{
+		putchar("\n");
+		return (0);
+	}
+}
+
