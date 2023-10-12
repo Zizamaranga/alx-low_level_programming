3ca404096201ad127db9e982ca193845f6c1c6bd
Zizamaranga <marangaziza@gmail.com>
2023-10-13 01:14:57 +0300

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ *
+ *
+ * @n: Number of times
+ *
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+}
