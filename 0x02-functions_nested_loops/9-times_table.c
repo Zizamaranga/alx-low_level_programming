1f3118a82d8f43bd00ef88978a44bf4bedd9d00b
Zizamaranga <marangaziza@gmail.com>
2023-10-12 20:42:10 +0300

    f

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - entry point
+ *
+ *
+ *
+ *
+ * Return : Always 0 ( success)
+ *
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (b == 0)
+				_putchar(c + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (c < 10)
+					_putchar(' ');
+				else
+					_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
+
