95e364b5893167bab7ec31cb42f330654c38a6be
Zizamaranga <marangaziza@gmail.com>
2023-10-12 23:37:40 +0300

    d

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	_putchar(c);
+	return (0);
+}
+
