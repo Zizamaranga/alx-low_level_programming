c3fb0bae3356978b23c8dd4b4c2f00a0ee059099
Zizamaranga <marangaziza@gmail.com>
2023-10-12 17:17:16 +0300

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * void - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)i;
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	int ch = 'a';
-	int ret;
+	char c;
+	int i;
 
-	ret = print_alphabet_x10;
-
-	while(ch <=  'z')
+	for (i = 0; i < 10; i++)
 	{
-		for (int ch = 0; ch <10; ch++);
-		putchar(ch);
-		return (0);
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
 	}
 }
+
