48132c0f29f75548593080d22adee5e62c556785
Zizamaranga <marangaziza@gmail.com>
2023-10-12 17:45:21 +0300

    d

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: checks letters
+ *
+ * Return: Always 1 or 0 (Success)
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 'a' && c >= 'z')
+		return (1);
+	else if (c <= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
