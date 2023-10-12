9069157ae81fc845afa5bd13e498b3dd3d512735
Zizamaranga <marangaziza@gmail.com>
2023-10-12 17:50:24 +0300

    d

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,7 @@
 
 int _isalpha(int c)
 {
-	if (c <= 'a' && c >= 'z')
-		return (1);
-	else if (c <= 'A' && c >= 'Z')
+	if ((c <= 'a' && c >= 'z') || (c <= 'A' && c >= 'Z'))
 		return (1);
 	else
 		return (0);
