52114026e58c1f7d73c2764b5ef5904fa66244db
Zizamaranga <marangaziza@gmail.com>
2023-10-12 19:00:57 +0300

    d

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ *
+ * @d: integer
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int print_last_digit(int d)
+{
+	int r;
+
+	if (d < 0)
+
+	{
+		r = -d % 10;
+	}
+	else
+	{
+		r = d % 10;
+	}
+
+	_putchar(0 + r);
+	return (r);
+}
