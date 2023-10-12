2d91a3812247b74812f2470c27d43399bebcd4b7
Zizamaranga <marangaziza@gmail.com>
2023-10-12 20:59:17 +0300

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds integers
+ * @a: First integer
+ * @b: second integer
+ * Return: Always 0 (Success)
+ *
+ */
+
+int add(int a, int b)
+{
+	int n = a + b;
+
+	return (n);
+}
