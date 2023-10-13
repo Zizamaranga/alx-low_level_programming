3eab465edc82f0dbab91af945433e4027fa869ac
Zizamaranga <marangaziza@gmail.com>
2023-10-13 14:08:36 +0300

    dd

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * largestPrimeFactor - Entry point
+ * @n: The number in question.
+ * Return: maxPrime (Success)
+ *
+ */
+
+long largestPrimeFactor(long n)
+{
+	long maxPrime = -1;
+
+	while (n % 2 == 0)
+
+	{
+		maxPrime == 2;
+		n >>= 1;
+	}
+
+	for (long i = 3; i * i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			maxPrime = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		maxPrime = n;
+	}
+	return (maxPrime);
+}
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	long num = 612852475143;
+
+	long result = largestPrimeFactor(num);
+
+	printf("%ld\n", result);
+	return (0);
+}
