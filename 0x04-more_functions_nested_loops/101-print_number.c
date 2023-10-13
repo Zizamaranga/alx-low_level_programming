973cca06eab886e44e1b5991c87f719bf181ec65
Zizamaranga <marangaziza@gmail.com>
2023-10-13 14:22:40 +0300

    a

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n: The printed number
+ *
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
