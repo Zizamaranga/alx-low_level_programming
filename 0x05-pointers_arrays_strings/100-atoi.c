a64f843a156f9d05549103a4e540e90937354cfe
Zizamaranga <marangaziza@gmail.com>
2023-10-17 00:06:50 +0300

    n

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+			{
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (result * sign);
+}
