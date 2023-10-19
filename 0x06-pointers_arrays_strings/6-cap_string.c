f3465abe4dfc85f6fd76874abed9bb3437865440
Zizamaranga <marangaziza@gmail.com>
2023-10-20 00:59:18 +0300

    s

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to capitalize
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+
+	return (str);
+}
+
