16beba07857d203c14c899ce05699f64c01f9b9e
Ziza Maranga <marangaziza@gmail.com>
2023-11-03 00:39:19 +0300

    f

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the amount of memory allocated
+ *
+ * Return: Success, pointer to allocated memory.
+ * Failure, normal process termination with status value 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+My readme for more malloc files
+
