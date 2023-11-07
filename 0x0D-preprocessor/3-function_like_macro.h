6a7d4d0c91ab7161665623e4b0e2c7abd18461c7
Ziza Maranga <marangaziza@gmail.com>
2023-11-08 01:15:38 +0300

    a

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,15 +1,8 @@
-#include <stdio.h>
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-/*
- * main - Entry point
- * Return: 0 success
- *
- */
-int main(void)
-{
-	int n = -42;
-	int abs_n = ABS(n);
-	printf("The absolute value of %d is %d\n", n, abs_n);
-	return (0);
-}
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? (-x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
+
 
