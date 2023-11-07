8420b048e79834d25807a21e9b6e4262badc2e32
Ziza Maranga <marangaziza@gmail.com>
2023-11-08 01:17:27 +0300

    s

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,16 +1,7 @@
-#include <stdio.h>
+#ifndef SUM_H
+#define SUM_H
 
 #define SUM(x, y) ((x) + (y))
-/*
- * main - Entry point
- * Return: 0 Always
- */
-int main(void)
-{
-	int a = 5;
-	int b = 3;
-	int r = SUM(a, b);
-	printf("The sum of %d and %d is %d\n", a, b, r);
-	return (0);
-}
+
+#endif /* SUM_H */
 
