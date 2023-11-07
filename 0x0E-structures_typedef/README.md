664028b7c26c97b4bd2eb53d7ae55e812821953d
Ziza Maranga <marangaziza@gmail.com>
2023-11-07 23:56:27 +0300

    s

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+My readme for structures and type def.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - struct that representing chars of dog.
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ * Description: This struct represents the characteristics 
+ * age, and owner's name.
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
