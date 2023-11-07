079a04246e7bd69e4537c076f010b9095c1d6b10
Ziza Maranga <marangaziza@gmail.com>
2023-11-08 00:42:13 +0300

    w

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,6 +11,14 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void print_dog(struct dog *d);
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 #endif /* DOG_H */
 
 
