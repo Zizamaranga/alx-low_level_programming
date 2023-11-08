ee6c636a38aca36a0ca7c8d7b34c3fc24775b1ea
Ziza Maranga <marangaziza@gmail.com>
2023-11-08 23:13:43 +0300

    g

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_name - Prints name
+ * @name: Pointer to name to print
+ * @f: Pointer to function
+ * that prints character string
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
+
