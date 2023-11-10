eae75b2e64f93ad6daccaf03afae341d0e33b4f3
Ziza Maranga <marangaziza@gmail.com>
2023-11-10 22:16:14 +0300

    s

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,72 +1,15 @@
 #include "function_pointers.h"
-
-
+#include <stddef.h>
 /**
- * array_iterator - Executes a function given as a parameter on each element of an array.
- * @array: Pointer to the array
- * @size: Size of the array
- * @action: Pointer to the function to be used
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: Pointer to a function that prints a character string.
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void print_name(char *name, void (*f)(char *))
 {
-	size_t i;
-
-	if (array && action)
+	if (name != NULL && f != NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		f(name);
 	}
 }
-
-/**
- * print_elem - Prints an integer
- * @elem: The integer to print
- *
- * Return: Nothing.
- */
-void print_elem(int elem)
-{
-	/* Convert integer to string manually */
-	if (elem < 0)
-	{
-		_putchar('-');
-		elem *= -1;
-	}
-	if (elem / 10)
-	{
-		print_elem(elem / 10);
-	}
-	_putchar(elem % 10 + '0');
-}
-
-/**
- * print_elem_hex - Prints an integer in hexadecimal
- * @elem: The integer to print
- *
- * Return: Nothing.
- */
-void print_elem_hex(int elem)
-{
-	int temp;
-	_putchar('0');
-	_putchar('x');
-	if (elem == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (elem != 0)
-	{
-		temp = elem % 16;
-		elem /= 16;
-		if (temp < 10)
-			_putchar(temp + '0');
-		else
-			_putchar(temp - 10 + 'a');
-	}
-}
-
-
 
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <stdio.h>
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
+#include <stddef.h>
+
 int _putchar(char c);
 
-#endif
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_elem(int elem);
+void print_elem_hex(int elem);
+
+#endif /* FUNCTION_POINTERS_H */
+
