89af250948ed0fabbc7735d47f09267d6cd4dc7d
Ziza Maranga <marangaziza@gmail.com>
2023-11-10 22:41:41 +0300

    d

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,5 +7,6 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem(int elem);
 void print_elem_hex(int elem);
 void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* FUNCTION_POINTERS_H */
 
