0f29086a86bb34e89d81274da8a90fb101962e21
Ziza Maranga <marangaziza@gmail.com>
2023-11-10 22:28:09 +0300

    s

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,12 +2,10 @@
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
-
 int _putchar(char c);
-
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem(int elem);
 void print_elem_hex(int elem);
-
+void print_name(char *name, void (*f)(char *));
 #endif /* FUNCTION_POINTERS_H */
 
