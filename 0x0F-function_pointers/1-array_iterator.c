fee86cb874179727d3d8aff0047c38479ec36159
Ziza Maranga <marangaziza@gmail.com>
2023-11-10 22:32:02 +0300

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -18,41 +17,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 			action(array[i]);
 		}
 	}
-}
-
-/**
- * print_elem - Prints an integer
- * @elem: The integer to print
- *
- * Return: Nothing.
- */
-void print_elem(int elem)
-{
-	printf("%d\n", elem);
-}
-
-/**
- * print_elem_hex - Prints an integer in hexadecimal
- * @elem: The integer to print
- *
- * Return: Nothing.
- */
-void print_elem_hex(int elem)
-{
-	printf("0x%x\n", elem);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int array[5] = {0, 98, 402, 1024, 4096};
-
-	array_iterator(array, 5, &print_elem);
-	array_iterator(array, 5, &print_elem_hex);
-	return (0);
 }
 
